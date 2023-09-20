c4c047e75d7835a23c105f70aff5fce0b25721d6
MarieStLaurent <marie.stl@gmail.com>
2023-09-20 16:32:47 -0400

    friends frames and bold dataloader

---
+++ cneuromax/deeplearning/experiment/friends_video_transformer/datamodule/__init__.py
@@ -0,0 +1,11 @@
+"""."""
+
+from cneuromax.deeplearning.experiment.friends_video_transformer.datamodule.friends_datamodule import (
+    FriendsDataModule,
+    FriendsDataModuleConfig,
+)
+
+__all__ = [
+    "FriendsDataModule",
+    "FriendsDataModuleConfig",
+]
