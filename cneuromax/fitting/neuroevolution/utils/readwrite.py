dc2a068a34036c429dbb9c8662ccb396475f4b71
Maximilien Le Clei <maximilienleclei@gmail.com>
2023-12-01 18:02:02 +0000

    readwrite numpy -> lists

--- cneuromax/fitting/neuroevolution/utils/readwrite.py
+++ cneuromax/fitting/neuroevolution/utils/readwrite.py
@@ -1,13 +1,13 @@
 """File reading and writing utilities for Neuroevolution fitting."""
 
+import pickle
 from pathlib import Path
 from typing import Annotated as An
 
-import numpy as np
-
+from cneuromax.fitting.neuroevolution.agent.singular.base import (
+    BaseSingularAgent,
+)
 from cneuromax.fitting.neuroevolution.utils.type import (
-    agents_batch_type,
-    agents_type,
     generation_results_batch_type,
     generation_results_type,
 )
@@ -16,19 +16,20 @@ from cneuromax.utils.mpi import retrieve_mpi_variables
 
 
 def load_state(
-    agents_batch: agents_batch_type,
     prev_num_gens: An[int, ge(0)],
+    len_agents_batch: An[int, ge(1)],
 ) -> tuple[
+    list[list[BaseSingularAgent]],  # agents_batch
     generation_results_type | None,  # generation_results
     An[int, ge(0)] | None,  # total_num_env_steps
 ]:
     """Load a previous experiment state from disk.
 
     Args:
-        agents_batch: See return value `agents_batch` in\
-            :func:`cneuromax.fitting.neuroevolution.utils.initialize`.
         prev_num_gens: See\
             :paramref:`~cneuromax.fitting.neuroevolution.fit.NeuroevolutionFittingHydraConfig.prev_num_gens`.
+        len_agents_batch: Number of agents per population in the batch\
+            maintained by each process.
 
     Returns:
         agents_batch: See return value `agents_batch` in\
@@ -40,26 +41,32 @@ def load_state(
     """
     comm, rank, size = retrieve_mpi_variables()
     if rank == 0:
-        path = Path.cwd() / f"/{prev_num_gens}/state.npz"
+        path = Path.cwd() / f"/{prev_num_gens}/state.pkl"
         if not path.exists():
             error_msg = f"No saved state found at {path}."
             raise FileNotFoundError(error_msg)
-        state = np.load(file=path, allow_pickle=True)
-        agents: agents_type = state["agents"]
-        generation_results: generation_results_type = state[
-            "generation_results"
+        with path.open(mode="rb") as f:
+            state = pickle.load(file=f)
+        agents: list[list[BaseSingularAgent]] = state[0]
+        generation_results: generation_results_type = state[1]
+        total_num_env_steps: int = state[2]
+        batched_agents: list[list[list[BaseSingularAgent]]] = [
+            agents[i * len_agents_batch : (i + 1) * len_agents_batch]
+            for i in range(size)
         ]
-        total_num_env_steps = int(state["total_num_env_steps"])
-    comm.Scatter(sendbuf=None if rank != 0 else agents, recvbuf=agents_batch)
+    # `comm.scatter` argument `sendobj` is wrongly typed. `[]` is the
+    # workaround for not being able to set it to `None`.
+    # See https://github.com/mpi4py/mpi4py/issues/434
+    agents_batch = comm.scatter(sendobj=[] if rank != 0 else batched_agents)
     return (
+        agents_batch,
         None if rank != 0 else generation_results,
         None if rank != 0 else total_num_env_steps,
     )
 
 
 def save_state(
-    agents: agents_type | None,
-    agents_batch: agents_batch_type,
+    agents_batch: list[list[BaseSingularAgent]],
     generation_results: generation_results_batch_type | None,
     total_num_env_steps: An[int, ge(0)] | None,
     curr_gen: int,
@@ -67,8 +74,6 @@ def save_state(
     """Dump the current experiment state to disk.
 
     Args:
-        agents: See return value `agents` in\
-            :func:`cneuromax.fitting.neuroevolution.utils.initialize`.
         agents_batch: See return value `agents_batch` in\
             :func:`cneuromax.fitting.neuroevolution.utils.initialize`.
         generation_results: See return value `generation_results` in\
@@ -78,21 +83,26 @@ def save_state(
         curr_gen: The current generation number.
     """
     comm, rank, _ = retrieve_mpi_variables()
-    comm.Gather(sendbuf=agents_batch, recvbuf=agents)
+    batched_agents: list[list[list[BaseSingularAgent]]] | None = comm.gather(
+        sendobj=agents_batch,
+    )
     if rank != 0:
         return
-    # `agents`, `generation_results`, and `total_num_env_steps` are only
-    # `None` when `rank != 0`. The following `assert` statements are for
-    # static type checking reasons and have no execution purposes.
-    assert agents  # noqa: S101
+    # `batched_agents`, `generation_results`, and `total_num_env_steps`
+    # are only  `None` when `rank != 0`. The following `assert`
+    # statements are for static type checking reasons and have no
+    # execution purposes.
+    assert batched_agents  # noqa: S101
     assert generation_results  # noqa: S101
     assert total_num_env_steps  # noqa: S101
+    agents: list[list[BaseSingularAgent]] = []
+    for agent_batch in batched_agents:
+        agents = agents + agent_batch
     path = Path.cwd() / f"/{curr_gen}/"
     if not path.exists():
         path.mkdir(parents=True)
-    np.savez(
-        file=path / "state",
-        agents=agents,
-        generation_results=generation_results,
-        total_num_env_steps=total_num_env_steps,
-    )
+    with path.open(mode="wb") as f:
+        pickle.dump(
+            obj=[agents, generation_results, total_num_env_steps],
+            file=f,
+        )
