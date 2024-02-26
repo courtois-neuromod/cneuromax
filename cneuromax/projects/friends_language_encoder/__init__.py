f1882e3dc557826396624eddc4b41e580598df9f
complexbrains <bilginlab@gmail.com>
2024-02-26 19:27:00 +0000

    Add version 26.02

---
+++ cneuromax/projects/friends_language_encoder/__init__.py
@@ -0,0 +1,62 @@
+"""Friends language finetuning ``project``."""
+
+from hydra_zen import ZenStore
+from transformers import AutoModelForMaskedLM
+
+from cneuromax.fitting.deeplearning.runner import DeepLearningTaskRunner
+from cneuromax.utils.hydra_zen import fs_builds
+
+from .datamodule import (
+    FriendsDataModule,
+    FriendsDataModuleConfig,
+)
+from .litmodule import (
+    FriendsFinetuningModel,
+    FriendsLitModuleConfig,
+)
+
+__all__ = [
+    "TaskRunner",
+    "FriendsDataModule",
+    "FriendsDataModuleConfig",
+    "FriendsFinetuningModel",
+    "FriendsLitModuleConfig",
+]
+
+
+class TaskRunner(DeepLearningTaskRunner):
+    """``project`` ``task`` runner."""
+
+    @classmethod
+    def store_configs(cls: type["TaskRunner"], store: ZenStore) -> None:
+        """Stores :mod:`hydra-core` ``project`` configs.
+
+        Args:
+            store: See :paramref:`~.BaseTaskRunner.store_configs.store`.
+        """
+        super().store_configs(store=store)
+        store(name="model_name")
+        store(
+            fs_builds(
+                FriendsDataModule,
+                config=FriendsDataModuleConfig(),
+            ),
+            name="friends_language_encoder",
+            group="datamodule",
+        )
+        store(
+            fs_builds(
+                FriendsFinetuningModel,
+                config=FriendsLitModuleConfig(),
+            ),
+            name="friends_language_encoder",
+            group="litmodule",
+        )
+        store(
+            fs_builds(
+                AutoModelForMaskedLM.from_pretrained,
+                pretrained_model_name_or_path="${model_name}",
+            ),
+            name="friends_language_encoder",
+            group="litmodule/nnmodule",
+        )
---
+++ cneuromax/projects/friends_language_encoder/litmodule.py
@@ -0,0 +1,67 @@
+""":class:`FriendsFinetuningModel`."""
+
+from dataclasses import dataclass
+from typing import Annotated as An
+from typing import Any
+
+from jaxtyping import Num
+from torch import Tensor
+
+from cneuromax.fitting.deeplearning.litmodule import (
+    BaseLitModule,
+)
+from cneuromax.utils.beartype import one_of
+
+
+@dataclass
+class FriendsLitModuleConfig:
+    """Holds :class:`FriendsLitModule` config values.
+
+    Args:
+        layer_name: layer to unfreeze
+    """
+
+    layer_name: str = "${layer_name}"
+
+
+class FriendsFinetuningModel(BaseLitModule):
+    """``project`` :class:`~BaseLitModule`."""
+
+    def __init__(
+        self: "FriendsFinetuningModel",
+        *args: Any,  # noqa: ANN401
+        **kwargs: Any,  # noqa: ANN401
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.config: FriendsLitModuleConfig
+        for name, param in self.nnmodule.named_parameters():
+            if self.config.layer_name in name:
+                param.requires_grad = True
+            else:
+                param.requires_grad = False
+
+    def step(
+        self: "FriendsFinetuningModel",
+        batch: dict[str, Num[Tensor, " ..."]],
+        stage: An[str, one_of("train", "val", "test", "predict")],
+    ) -> Num[Tensor, " ..."]:
+        """Inputs a batch and returns the loss or logits.
+
+        Args:
+            batch: See :paramref:`~.BaseLitModule.x_step.batch`.
+            stage: See :paramref:`~.BaseLitModule.x_step.stage`.
+
+        Returns:
+            The loss if ``stage`` is ``train``, ``val``, or ``test``,\
+                otherwise the logits.
+        """
+        out = self.nnmodule(
+            input_ids=batch["input_ids"],
+            attention_mask=batch["attention_mask"],
+            labels=batch["labels"],
+        )
+        out: Tensor = (
+            out["loss"] if stage in ["train", "val", "test"] else out["logits"]
+        )
+        return out
