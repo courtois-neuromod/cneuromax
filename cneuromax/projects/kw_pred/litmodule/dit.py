6175321ce773ab28d2c81bd925f24d5892c777bb
Maximilien Le Clei <maximilienleclei@gmail.com>
2024-03-20 14:13:35 +0000

    clean up input embedding, start conditionning embedding

--- cneuromax/projects/kw_pred/litmodule/dit.py
+++ cneuromax/projects/kw_pred/litmodule/dit.py
@@ -2,9 +2,65 @@
 
 from typing import Any
 
+from jaxtyping import Float
 from torch import Tensor, nn
 
-from cneuromax.projects.kw_pred.DiT.models import DiT
+from cneuromax.projects.kw_pred.dit.models import DiT
+
+
+class OneDPatchEmbed(nn.Module):
+    """Custom :attr:`.DiT.x_embedder`.
+
+    Meant to replace :class:`timm.models.vision_transformer.PatchEmbed`
+    given that we input 1D data as opposed to 2D images.
+    """
+
+    def __init__(
+        self: "OneDPatchEmbed",
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int,
+    ) -> None:
+        super().__init__()
+        self.proj = nn.Conv1d(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+        )
+
+    def forward(
+        self: "OneDPatchEmbed",
+        x: Float[Tensor, " batch_size in_channels seq_len"],
+    ) -> Float[Tensor, " batch_size num_patches out_channels"]:
+        """Original input -> Input for the transformer."""
+        x: Float[Tensor, " batch_size out_channels num_patches"] = self.proj(x)
+        x: Float[Tensor, " batch_size num_patches out_channels"] = x.transpose(
+            dim0=-1,
+            dim1=-2,
+        )
+        return x
+
+
+class STFTEmbedder(nn.Module):
+    """Custom :attr:`~DiT.y_embedder`.
+
+    Meant to replace :class:`.dit.models.LabelEmbedder` given that our
+    conditioning data is the STFT of the audio signal rather than the
+    class labels.
+    """
+
+    def __init__(self: "STFTEmbedder") -> None:
+        super().__init__()
+        self.embedding_table = None
+
+    def forward(
+        self: "STFTEmbedder",
+        x: Float[Tensor, " batch_size in_channels seq_len"],
+    ) -> Float[Tensor, " batch_size num_patches out_channels"]:
+        """Forward pass."""
+        return x
 
 
 class CustomDiT(DiT):
@@ -19,17 +75,10 @@ class CustomDiT(DiT):
     ) -> None:
         """Initialize the model."""
         super().__init__(input_size, hidden_size, *args, **kwargs)  # type: ignore[no-untyped-call]
-
-        class TransposeMin1Min2(nn.Module):
-            def forward(self: "TransposeMin1Min2", x: Tensor) -> Tensor:
-                return x.transpose(-1, -2)
-
-        self.x_embedder = nn.Sequential(
-            nn.Conv1d(
-                in_channels=self.in_channels,
-                out_channels=hidden_size,
-                kernel_size=self.patch_size,
-                stride=self.patch_size,
-            ),
-            TransposeMin1Min2(),
-        )  # BS x in_channels x seq_len -> BS x num_patches x hidden_size
+        self.x_embedder = OneDPatchEmbed(
+            in_channels=self.in_channels,
+            out_channels=hidden_size,
+            kernel_size=self.patch_size,
+            stride=self.patch_size,
+        )
+        self.y_embedder = STFTEmbedder()
