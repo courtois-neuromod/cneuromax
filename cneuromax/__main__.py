5fc04e6d166ee459ce9c53dc3b985a2de2b77514
Maximilien Le Clei <maximilienleclei@gmail.com>
2024-09-19 17:17:13 +0000

    add `eval` resolver

--- cneuromax/__main__.py
+++ cneuromax/__main__.py
@@ -1,10 +1,13 @@
 """:mod:`cneuromax` entrypoint."""
 
+from omegaconf import OmegaConf
+
 from cneuromax.runner import BaseTaskRunner
 from cneuromax.utils.runner import get_task_runner_class
 from cneuromax.utils.wandb import login_wandb
 
 if __name__ == "__main__":
     TaskRunner: type[BaseTaskRunner] = get_task_runner_class()
+    OmegaConf.register_new_resolver("eval", eval)
     login_wandb()
     TaskRunner.store_configs_and_run_task()
